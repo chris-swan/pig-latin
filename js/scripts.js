c248e3a90c445a8c515637ed6765785ff9533347
chris swan <chris@chrisswan.com>
2015-09-11 11:03:55 -0700

    Add inital files and write first test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,99 +1,50 @@
-var pigLatin = function(userInput) {
+var wordToFind = function(userPhrase, oldWord) {
 
-  var words = userInput.split(" ");
-  var translatedWords = [];
-  words.forEach(function(word) {
-    var wordStatic = word.slice();
-    while (!isVowel(word[0])) {
-      if (word.slice(0,2) === "qu") {
-        word = word.slice(2) + word.slice(0,2);
-      } else if (wordStatic[0] !== "y" && word[0] === "y"){
-        break;
-      } else {
-        word = word.slice(1) + word[0];
-      }
-    }
-    translatedWords.push(word + "ay");
+  var words = userPhrase.split(" ");
+  var newWord;
+  var newWords = [];
+  words.forEach(function(words) {
+    //var wordStatic = word.slice();
+    var replacedWords = words.replace(oldWord, newWord)
+    // while (!isVowel(word[0])) {
+    //   if (word.slice(0,2) === "qu") {
+    //     word = word.slice(2) + word.slice(0,2);
+    //   } else if (wordStatic[0] !== "y" && word[0] === "y"){
+    //     break;
+    //   } else {
+    //     word = word.slice(1) + word[0];
+    //   }
+    // }
+    newWords.push(newWord);
   });
 
-  return translatedWords.join(" ");
+  return newWords.join(" ");
 }
-
-var isVowel = function(userInputLetter) {
-  var vowels = ["a", "e", "i", "o", "u"]
-  var vowelValue = false;
-  vowels.forEach(function(vowel) {
-    if (userInputLetter === vowel) {
-      vowelValue = true;
-    }
-  });
-
-  return vowelValue;
-}
-
-$(document).ready(function() {
-  $("form#pig_latin").submit(function(event) {
-    var user_input_form = String($("input#user_input").val());
-    var result = pigLatin(user_input_form);
-
-    $(".result").text(result);
-
-    event.preventDefault();
-
-  });
-});
-
-// Other attempts in work process:
-// var pigLatin = function(userInput) {
-//   var letters = userInput.split("");
-//     letters.forEach(function(letter)) {
-//       if ((userInput[0] != "a") && (userInput[0] != "e") && (userInput[0] != "i") && (userInput[0] != "o") && (userInput[0] != "u")) {
-//         userInput = userInput.slice(1) + userInput[0];
-//       }
-//     }
 //
-//   return userInput + "ay";
-// }
+// $(document).ready(function() {
+//   $("form#pig_latin").submit(function(event) {
+//     var user_input_form = String($("input#user_input").val());
+//     var result = findWord(user_input_form);
+//
+//     $(".result").text(result);
+//
+//     event.preventDefault();
+//
+//   });
+// });
 
 
-// var pigLatin = function(userInput) {
-//   var vowels = ["a", "e", "i", "o", "u"];
-//   for (var i = 0; i < 5; i++) {
-//     if (userInput[i] === "a") {
-//       break;
-//     } else if (userInput[i] === "e") {
-//       break;
-//     } else if (userInput[i] === "i") {
-//       break;
-//     } else if (userInput[i] === "o") {
-//       break;
-//     } else if (userInput[i] === "u") {
-//       break;
+// var words = userInput.split(" ");
+// var ReplacedWords = [];
+// words.forEach(function(word) {
+//   var wordStatic = word.slice();
+//   for each word in words
+//     if
 //     } else {
-//       userInput = userInput.slice(1) + userInput[0];
+//       word = word.slice(1) + word[0];
 //     }
 //   }
-
-//   return userInput + "ay";
-// }
-
-// var pigLatin = function(userInput) {
-//   var consonants = ["q", "w", "r", "t", "y", "p", "s", "d", "f", "g", "j", "h", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
-//   var vowels = ["a", "e", "i", "o", "u"];
-//   for (var i = 0; i <= 4; i++) {
-//     vowels.forEach(function(vowel) {
-//       if (userInput.slice(i, i + 1) === vowel) {
-//         debugger;
-//         userInput = userInput + "ay";
-//       } else {
-//         consonants.forEach(function(consonant) {
-//             if (userInput.slice(0, 1) === consonant) {
-//               userInput = userInput.slice(1) + consonant;
-//               console.log(userInput);
-//             }
+//   translatedWords.push(newWord);
+// });
 //
-//         });
-//       }
-//     });
-//   }
-// }
+// return ReplacedWords.join(" ");
--- spec/specs.js
+++ spec/specs.js
@@ -1,35 +1,5 @@
-describe('pigLatin', function() {
-  it("adds 'ay' to the end of the word", function() {
-    expect(pigLatin("order")).to.equal("orderay");
-  });
-
-  it("moves the consonant to the end of the word with 'ay'", function() {
-    expect(pigLatin("marker")).to.equal("arkermay");
-  });
-
-  it("moves multiple consonants at the start of a word to the end with 'ay'", function() {
-    expect(pigLatin("start")).to.equal("artstay");
-  });
-
-  it("moves 'u' when it is following a 'q' at the start of the word", function() {
-    expect(pigLatin("squeel")).to.equal("eelsquay");
-  });
-
-  it("handles y as a vowel when it is not the first letter", function() {
-    expect(pigLatin("dyson")).to.equal("ysonday");
-  });
-
-  it("Translates a sentance/multiple words correctly to pig latin", function() {
-    expect(pigLatin("translate this sentance")).to.equal("anslatetray isthay entancesay");
-  } )
-});
-
-describe('isVowel', function() {
-  it("returns true if the input is a vowel", function() {
-    expect(isVowel("a")).to.equal(true);
-  });
-
-  it("returns false if the input is a not a vowel", function() {
-    expect(isVowel("c")).to.equal(false);
+describe('wordToFind', function() {
+  it("replaces a single word", function() {
+    expect(wordToFind("order","order")).to.equal("law");
   });
 });
